ffe3bf064ab17d8127d02dce50c99af4fc6bd92b
guigomcha <guillermo.chavez@enel.com>
2020-04-05 18:57:38 +0200

    first migration to db for gameplay app

---
+++ excel2web/gameplay/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Game(models.Model):
+    # ForeignKey --> one to many relationship
+    # CASCADE--> If the game is deleted, all moves linked will be as well
+    first_player = models.ForeignKey(User, related_name="games_first_player", on_delete=models.CASCADE)
+    second_player = models.ForeignKey(User, related_name="games_second_player",  on_delete=models.CASCADE)
+    start_time = models.DateTimeField(auto_now_add=True)
+    last_time = models.DateTimeField(auto_now=True)
+
+
+class Move(models.Model):
+    """
+    Define fields. They are all not null by default unless specified. For that we should give an initial value
+    """
+    # Primary key included by default
+    x = models.IntegerField()
+    y = models.IntegerField()
+    comment = models.CharField(max_length=300, blank=True)
+    by_first_player = models.BooleanField()
+    # Many to one relationship
+    game = models.ForeignKey(Game,  on_delete=models.CASCADE)
