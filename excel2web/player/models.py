dad28f9e7e60c74446228911919b501ce8ed02b4
guigomcha <guillermo.chavez@enel.com>
2020-04-10 17:23:32 +0200

    added crispy style to forms

--- excel2web/player/models.py
+++ excel2web/player/models.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import User
 
 
 class Invitation(models.Model):
-    from_user = models.ForeignKey(User, related_name="invitations_sent", on_delete=models.CASCADE)
+    from_user = models.ForeignKey(User, related_name="invitations_sent", on_delete=models.CASCADE,
+                                  verbose_name="user to invite", help_text="some extra help text")
     to_user = models.ForeignKey(User, related_name="invitations_received", on_delete=models.CASCADE)
-    message = models.CharField(max_length=300)
+    message = models.CharField(max_length=300, blank=True, verbose_name="Optional message",
+                               help_text="some extra help text")
     timestamp = models.DateTimeField(auto_now_add=True)
