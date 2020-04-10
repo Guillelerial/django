5dbc9d7c9401c98bfa05867230be62cb6e1672e4
guigomcha <guillermo.chavez@enel.com>
2020-04-10 16:33:19 +0200

    added get/post logic to the invitation view method.

--- excel2web/player/views.py
+++ excel2web/player/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from gameplay.models import Game
@@ -6,7 +6,7 @@ from gameplay.models import Game
 from . import models
 from . import forms
 
-# Create your views here.
+
 @login_required
 def home(request):
     # Path is relative to the template folder on this app
@@ -14,7 +14,17 @@ def home(request):
     active_games = my_games.active()
     return render(request, "player/home.html", {'games': my_games, 'active_games': active_games})
 
+
 @login_required
 def new_invitation(request):
-    form = forms.InvitationForm()
+    if request.method == 'POST':
+        # Model on the Form now takes info from the invitation model we are passing
+        invitation = models.Invitation(from_user=request.user)
+        form = forms.InvitationForm(instance=invitation, data=request.POST)
+        if form.is_valid():
+            form.save()  # Adds data to the db
+            return redirect('player_home')
+    else:
+        form = forms.InvitationForm()
+    # Form will be the same as we received in case validation was not successful
     return render(request, "player/new_invitation_form.html", {'form': form})
