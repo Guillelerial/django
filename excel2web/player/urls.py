ab994cf7f87f9a72983bcad122cc77dbcde727b0
guigomcha <guillermo.chavez@enel.com>
2020-04-10 17:15:09 +0200

    added delivery and acceptance of invitations

--- excel2web/player/urls.py
+++ excel2web/player/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     url(r'home', views.home, name="player_home"),  # $ ensures that the path finishes here
     url(r'login$', LoginView.as_view(template_name="player/login_form.html"), name="player_login"),
     url(r'logout$', LogoutView.as_view(), name="player_logout"),
-    url(r'new_invitation$', views.new_invitation, name="player_new_invitation")
+    url(r'new_invitation$', views.new_invitation, name="player_new_invitation"),
+    # (?P<id>\d+)/$ name group. d+ = any number of digits. ?P<id> = group into id
+    url(r'accept_invitation/(?P<id>\d+)/$', views.accept_invitation, name="player_accept_invitation")
+
 ]
--- excel2web/player/views.py
+++ excel2web/player/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from gameplay.models import Game
@@ -12,7 +13,13 @@ def home(request):
     # Path is relative to the template folder on this app
     my_games = Game.objects.games_for_user(request.user)
     active_games = my_games.active()
-    return render(request, "player/home.html", {'games': my_games, 'active_games': active_games})
+    invitations = request.user.invitations_received.all()
+    return render(request, "player/home.html",
+                  {
+                      'games': my_games,
+                      'active_games': active_games,
+                      'invitations': invitations
+                   })
 
 
 @login_required
@@ -28,3 +35,20 @@ def new_invitation(request):
         form = forms.InvitationForm()
     # Form will be the same as we received in case validation was not successful
     return render(request, "player/new_invitation_form.html", {'form': form})
+
+
+@login_required
+def accept_invitation(request, id):
+    invitation = get_object_or_404(models.Invitation, pk=id)
+    if not request.user == invitation.to_user:
+        raise PermissionDenied
+    if request.method == 'POST' and "accept" in request.POST:
+        game = Game.objects.create(
+            first_player=invitation.to_user,
+            second_player=invitation.from_user
+        )
+        # game.save() is not implicit on the create call
+        invitation.delete()
+        return redirect('player_home')
+
+    return render(request, "player/accept_invitation_form.html", {'invitation': invitation})
